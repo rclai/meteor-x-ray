84eb900e52b796be005076b4606259275ff2daca
Richard Lai <it.rclai@gmail.com>
2015-02-12 22:44:07 -0500

    Added sync test

--- tests/lai:meteor-xray-tests.js
+++ tests/lai:meteor-xray-tests.js
@@ -1,4 +1,6 @@
 Tinytest.add('google title', function (test) {
+  // Meteor will complain about not running this
+  // in a Fiber, but will still pass
   xray('http://google.com')
     .select('title')
     .run(function (err, title) {
@@ -7,4 +9,22 @@ Tinytest.add('google title', function (test) {
         test.equal(title, expected);
       }
     });
+});
+
+Tinytest.add('google title synchronous', function (test) {
+  var future = new (Npm.require('fibers/future'))();
+
+  xray('http://google.com')
+    .select('title')
+    .run(function (err, title) {
+      if (!err) {
+        future.return(title);
+      } else {
+        future.throw(err);
+      }
+    });
+
+  // Google's title will get stored
+  var title = future.wait();
+  test.equal(title, 'Google');
 });
