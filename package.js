2757541b130aa4c2eeacbec8a31998895201bd47
Richard Lai <it.rclai@gmail.com>
2015-02-12 21:59:10 -0500

    Added package summary

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'lai:meteor-xray',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor wrapper for the NPM x-ray web scraper',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/rclai/meteor-x-ray.git',
   // By default, Meteor will default to using README.md for documentation.
